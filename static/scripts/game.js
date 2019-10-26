621329c042edd6df2e2fe7c45fe75f0d47f999d5
ei84136 <e.izaguirre0112@gmail.com>
2019-10-26 19:53:02 -0400

    done with js for displaying random item image

--- static/scripts/game.js
+++ static/scripts/game.js
@@ -8,52 +8,59 @@ function randomItemPic(){
   let item1Var = Math.floor(Math.random() * 6);
   let item2Var = Math.floor(Math.random() * 6);
 
+  console.log(item1Var);
+  console.log(item2Var);
+
   let equalFlag = (item1Var == item2Var)
 
-  while (equalFlag && True){
+  while (equalFlag){
     item2Var = Math.floor(Math.random() * 6);
     if (item1Var != item2Var){
-      equalFlag == True;
+      equalFlag = false;
     }
+    console.log(item2Var)
   }
 
   if (item1Var == 0){
-    item1Var = "../images/1.jpg";
+    item1Var = "../static/images/1.jpg";
   }
   else if (item1Var == 1){
-    item1Var = "../images/2.jpg";
+    item1Var = "../static/images/2.jpg";
   }
   else if (item1Var == 2) {
-    item1Var = "../images/3.jpg";
+    item1Var = "../static/images/3.jpg";
   }
   else if (item1Var == 3) {
-    item1Var = "../images/png";
+    item1Var = "../static/images/4.png";
   }
   else if (item1Var == 4) {
-    item1Var = "../images/5.jpg";
+    item1Var = "../static/images/5.jpg";
   }
   else if (item1Var == 5) {
-    item1Var = "../images/6.png";
+    item1Var = "../static/images/6.png";
   }
 
   if (item2Var == 0){
-    item2Var = "../images/1.jpg";
+    item2Var = "../static/images/1.jpg";
   }
   else if (item2Var == 1){
-    item2Var = "../images/2.jpg";
+    item2Var = "../static/images/2.jpg";
   }
   else if (item2Var == 2) {
-    item2Var = "../images/3.jpg";
+    item2Var = "../static/images/3.jpg";
   }
   else if (item2Var == 3) {
-    item2Var = "../images/4.png";
+    item2Var = "../static/images/4.png";
   }
   else if (item2Var == 4) {
-    item2Var = "../images/5.jpg";
+    item2Var = "../static/images/5.jpg";
   }
   else if (item2Var == 5) {
-    item2Var = "../images/6.png";
+    item2Var = "../static/images/6.png";
   }
+
+  console.log(item1Var)
+  console.log(item2Var)
 
   item1.src = item1Var;
   item2.src = item2Var;
